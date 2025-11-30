bf4c8d27409b3b09abec4c0c3afaed86fb528d7b
User <User@DESKTOP-JUG3TR6>
2025-11-30 14:08:55 -0500

    Add project files.

---
+++ API.W.Movies/DAL/Models/AuditBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+        [Key] //Este data annotation indica que el campo es la clave primaria
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate{ get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ API.W.Movies/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] //Este data annotation indica que el campo es obligatorio
+        [Display(Name= "Nombre de la Categoría")] // Me sirve para personalizar el nombre que se muestra en las vistas o mensaje error
+        public string Name { get; set; }
+    }
+}
