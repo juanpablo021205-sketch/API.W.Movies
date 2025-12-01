218e08a9cc9aaaf69e19f342a271b8e16f706a2c
JUAN PABLO PULGARIN RIOS <juanpulgarin1128554@correo.itm.edu.co>
2025-11-30 21:00:10 -0500

    Actulice la direccion de Server=(localdb)\\MSSQLLocalDB

--- API.W.Movies/DAL/Models/AuditBase.cs
+++ API.W.Movies/DAL/Models/AuditBase.cs
@@ -7,6 +7,6 @@ namespace API.W.Movies.DAL.Models
         [Key] //Este data annotation indica que el campo es la clave primaria
         public virtual int Id { get; set; }
         public virtual DateTime CreatedDate{ get; set; }
-        public virtual DateTime ModifiedDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
     }
 }
